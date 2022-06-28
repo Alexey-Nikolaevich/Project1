6ed4d59cdb71a00fc94ecdeada8f2205b6dd40b5
Alexey-Nikolaevich <anbardushko@miem.hse.ru>
2022-06-28 16:22:07 +0300

    General CleanUp2

--- Source/headers/Camera.h
+++ Source/headers/Camera.h
@@ -10,10 +10,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtx/rotate_vector.hpp>
-#include <glm/gtx/vector_angle.hpp>
 
 class Camera
 {
